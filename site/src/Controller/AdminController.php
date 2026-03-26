13d04c4147b06f9babada7ccc6a53ef70937fa1a
RIBOULEAU EVAN <evan.ribouleau@etu.univ-poitiers.fr>
2026-03-26 11:15:04 +0000

    Evan

--- site/src/Controller/AdminController.php
+++ site/src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Entity\Product;
 use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,6 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin', name: 'app_admin_')]
 final class AdminController extends AbstractController
 {
+    // pour ajouter un produit
     #[Route('/produit/ajouter', name: 'produit_add')]
     public function addProduct(Request $request, EntityManagerInterface $em): Response
     {
@@ -36,5 +39,43 @@ final class AdminController extends AbstractController
         return $this->render('admin/add_product.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+
+    // pour les clients
+    #[Route('/clients', name: 'manage_clients')]
+    public function listClients(UserRepository $userRepo): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_accueil');
+        }
+        $users = $userRepo->findAll();
+
+        return $this->render('admin/manage_clients.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/client/delete/{id}', name: 'client_delete', methods: ['POST'])]
+    public function deleteClient(User $client, Request $request, EntityManagerInterface $em): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_accueil');
+        }
+        if ($client === $this->getUser()) {
+            $this->addFlash('danger', 'Vous ne pouvez pas supprimer votre propre compte.');
+            return $this->redirectToRoute('app_admin_clients');
+        }
+        if (in_array('ROLE_SUPER_ADMIN', $client->getRoles())) {
+            $this->addFlash('danger', 'Impossible de supprimer un Super-Administrateur.');
+            return $this->redirectToRoute('app_admin_clients');
+        }
+        if ($this->isCsrfTokenValid('delete'.$client->getId(), $request->request->get('_token'))) {
+            $em->remove($client);
+            $em->flush();
+            $this->addFlash('success', 'Le compte de '.$client->getPrenom().' a bien été supprimé.');
+        }
+
+        return $this->redirectToRoute('app_admin_manage_clients');
     }
 }
